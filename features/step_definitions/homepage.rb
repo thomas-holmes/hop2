77a655019e67a5aa547c18b37e8239305a03e020
Thomas Holmes <thomas@holmes.io>
2013-08-15 15:48:26 -0400

    commit pending step

--- features/step_definitions/homepage.rb
+++ features/step_definitions/homepage.rb
@@ -6,7 +6,8 @@ Then(/^it should load$/) do
   expect(title).to eq("Shorty")
 end
 
-# Then(/^I should be able to submit a url to shorten$/) do
-#   fill_in 'short_url_url', with: "http://example.com"
-#   click_on 'Submit'
-# end
+Then(/^I should be able to submit a url to shorten$/) do
+  #fill_in 'short_url_url', with: "http://example.com"
+  #click_on 'Submit'
+  pending
+end
