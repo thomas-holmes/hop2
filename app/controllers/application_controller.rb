5a9cfaf38e998144b5f7bda8a6497eb0f15021eb
Thomas Holmes <thomas@devminded.com>
2013-12-22 21:29:05 -0500

    add custom 404 when short url isn't found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    render 'errors/not_found', status: 404
+  end
 end
