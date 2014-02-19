f7757740aee78abb10a6c267d2ec2590cc7d7d8e
Thomas Holmes <thomas@holmes.io>
2014-02-19 16:29:31 -0500

    Add scope that queries projection of url & clicks

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,3 +1,13 @@
 class Click < ActiveRecord::Base
   belongs_to :short_url
+
+  # Don't need this yet, but it might be handy later. Project all urls for a user and
+  # the amount of times clicked.
+  # scope :by_user, -> (user_id = nil) do
+  #   q = select("short_urls.url, count(clicks.id) as number_of_clicks")
+  #       .joins("left join short_urls on clicks.short_url_id = short_urls.id")
+  #       .group("url")
+  #   q = q.where("short_urls.user_id = ?", user_id) if user_id
+  #   q
+  # end
 end
